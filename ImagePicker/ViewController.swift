1bf7cf44fa117266b953afcb5cfdceb2bd16a790
Sanghun <po4865+github_sanghun@gmail.com>
2021-07-14 17:17:39 +0900

    initial commit - be aware that UIImagePickerController seems not working in ios simulator those upper than the version X(10)

---
+++ ImagePicker/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  ImagePicker
+//
+//  Created by Sanghun Park on 2021/07/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    lazy var imagePicker: UIImagePickerController = {
+        let picker: UIImagePickerController = UIImagePickerController()
+        picker.sourceType = .savedPhotosAlbum
+        picker.delegate = self
+        return picker
+    }()
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func touchUpSelectImageButton(_ sender: UIButton) {
+        self.present(self.imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let originalImage: UIImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.imageView.image = originalImage
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
+
